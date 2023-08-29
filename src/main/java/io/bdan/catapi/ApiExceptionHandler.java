e310042d3eff070ea74f8ee2ed4ff0caf6718dfa
bdanMusic <87788497+bdanMusic@users.noreply.github.com>
2023-08-29 16:26:26 +0200

    i honestly have no clue

--- src/main/java/io/bdan/catapi/ApiExceptionHandler.java
+++ src/main/java/io/bdan/catapi/ApiExceptionHandler.java
@@ -1,12 +1,11 @@
 package io.bdan.catapi;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ApiExceptionHandler implements ErrorController {
